f70d1bda6a19db49a8988f7659659a35cf0edd6d
Kirill <kirilwolf@gmail.com>
2022-01-19 20:41:14 +0200

    hw2
    сделал как понял ;)

---
+++ QuicStart/src/test/java/AppTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+
+
+@Test
+public void shouldAnswerWithTrue()
+{
+    System.out.println("AppTest");
+    assertTrue(true);
+}
+@Test
+    public void shouldAnswertWithTrue1()
+{
+    System.out.println("AppTest1");
+    assertTrue(1 == 2);
+}
+
+ }
---
+++ QuicStart/src/test/java/Apptest2.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+
+public class Apptest2
+{
+    @Test
+    public void shouldAnswertWithTrue()
+    {
+        System.out.println("Apptest2");
+        assertTrue(true);
+    }
+    @Test
+    public void shouldAnswertWithtrue()
+    {
+        System.out.println("Apptest.");
+        assertTrue(1==1);
+    }
+}
